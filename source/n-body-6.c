334ee27154abb2f76afcd39b2ec2bd24358362f3
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-05-21 13:11:41 +0200

    Commented kernel 6

--- source/n-body-6.c
+++ source/n-body-6.c
@@ -30,8 +30,8 @@ cl_mem makeBufferOnGPU(int length)
     cl_mem buffer = clCreateBuffer(
 			g_context,
             CL_MEM_READ_WRITE,
-            sizeof(cl_float3) * length, 
-			NULL, 
+            sizeof(cl_float3) * length,
+			NULL,
 			&error
 		);
 
@@ -56,7 +56,7 @@ void simulate_gravity(cl_float3* host_pos, cl_float3* host_speed, cl_float3* hos
     const float distance_to_nearest_star = 50;
 
 //vorige code, is vervangen door kernel
-/*
+/* FOR LUS 1
    for (int i = 0; i < length; ++i)
     {
         for (int j = 0; j < length; ++j)
@@ -103,13 +103,14 @@ void simulate_gravity(cl_float3* host_pos, cl_float3* host_speed, cl_float3* hos
 	ocl_err(clSetKernelArg(kernel_speed, arg_num++, sizeof(cl_mem), &gpu_speed));
 	ocl_err(clSetKernelArg(kernel_speed, arg_num++, sizeof(cl_mem), &gpu_dist));
 
+    // Kopieer buffers
 	ocl_err(clEnqueueWriteBuffer(g_command_queue, gpu_pos, CL_TRUE, 0, sizeof(cl_float3) * length,host_pos, 0, NULL, NULL));
 	ocl_err(clEnqueueWriteBuffer(g_command_queue, gpu_speed, CL_TRUE, 0, sizeof(cl_float3) * length,host_speed, 0, NULL, NULL));
 	ocl_err(clEnqueueWriteBuffer(g_command_queue, gpu_dist, CL_TRUE, 0, sizeof(cl_float3) * length,host_dist, 0, NULL, NULL));
 
 	// Call kernel 2D
 	size_t global_work_sizes_speed[] = {length,length};
-	time_measure_start("computation"); 
+	time_measure_start("computation");
 	ocl_err(clEnqueueNDRangeKernel(g_command_queue, kernel_speed, DIMENSION_2D, NULL, global_work_sizes_speed, NULL, 0, NULL, NULL));
 	ocl_err(clFinish(g_command_queue));
 	time_measure_stop_and_print("computation");
@@ -120,10 +121,9 @@ void simulate_gravity(cl_float3* host_pos, cl_float3* host_speed, cl_float3* hos
 	ocl_err(clEnqueueReadBuffer(g_command_queue, gpu_dist, CL_TRUE, 0, sizeof(cl_float3) * length, host_dist, 0, NULL, NULL));
 	time_measure_stop_and_print("data_transfer");
 
-	//EINDE VERVANGING FOR-LUS 1
 
-
-/*    for (int i = 0; i < length; ++i)
+/*  FOR LUS 2
+    for (int i = 0; i < length; ++i)
     {
         host_pos[i].s[0] += (host_speed[i].s[0] * delta_time) / distance_to_nearest_star;
         host_pos[i].s[1] += (host_speed[i].s[1] * delta_time) / distance_to_nearest_star;
@@ -140,12 +140,13 @@ void simulate_gravity(cl_float3* host_pos, cl_float3* host_speed, cl_float3* hos
     ocl_err(clSetKernelArg(kernel_pos, arg_num++, sizeof(cl_mem), &gpu_pos));
 	ocl_err(clSetKernelArg(kernel_pos, arg_num++, sizeof(cl_mem), &gpu_speed));
 
+    // Kopieer buffers
     ocl_err(clEnqueueWriteBuffer(g_command_queue, gpu_pos, CL_TRUE, 0, sizeof(cl_float3) * length,host_pos, 0, NULL, NULL));
     ocl_err(clEnqueueWriteBuffer(g_command_queue, gpu_speed, CL_TRUE, 0, sizeof(cl_float3) * length,host_speed, 0, NULL, NULL));
 
     // Call kernel 1D
     size_t global_work_sizes_pos[] = {length};
-    time_measure_start("computation"); 
+    time_measure_start("computation");
     ocl_err(clEnqueueNDRangeKernel(g_command_queue, kernel_pos, DIMENSION_1D, NULL, global_work_sizes_pos, NULL, 0, NULL, NULL));
     ocl_err(clFinish(g_command_queue));
     time_measure_stop_and_print("computation");
@@ -164,24 +165,28 @@ int main(int argc, char** argv) {
     }
     int length = atoi(argv[1]);
 
-	cl_platform_id platform = ocl_select_platform();	//GPU gerelateerde zaken
+	cl_platform_id platform = ocl_select_platform(); // Selecteer OpenCL platform + device ID
     cl_device_id device = ocl_select_device(platform);
-    init_ocl(device);
+    init_ocl(device); // Maak device ready
     create_program("kernel.cl", "");
 
+    // Maak kernel en vang eventuele errors op
 	cl_int error_pos,error_speed;
-    // cl_kernel kernel = clCreateKernel(g_program, "calc_pos", &error);
     cl_kernel kernel_pos = clCreateKernel(g_program, "calc_pos_a", &error_pos);
     cl_kernel kernel_speed  = clCreateKernel(g_program, "calc_speed_a", &error_speed);
-    //cl_kernel kernel = clCreateKernel(g_program, "calc_speed", &error);
     init_gl();
 
+    // Alloceer buffers op host
     cl_float3 *host_pos = malloc(sizeof(cl_float3) * length);
-    cl_float3 *host_speed = malloc(sizeof(cl_float3) * length);	
-    cl_float3 *host_dist = malloc(sizeof(cl_float3) * length);	
+    cl_float3 *host_speed = malloc(sizeof(cl_float3) * length);
+    cl_float3 *host_dist = malloc(sizeof(cl_float3) * length);
+
+    // Alloceer buffers op GPU
 	cl_mem gpu_pos = makeBufferOnGPU(length);
 	cl_mem gpu_speed = makeBufferOnGPU(length);
-	cl_mem gpu_dist = makeBufferOnGPU(length); //maken GPU buffers
+	cl_mem gpu_dist = makeBufferOnGPU(length);
+
+    // Init buffers op host
     for (int i = 0; i < length; ++i)
     {
         float offset;
@@ -205,13 +210,15 @@ int main(int argc, char** argv) {
         host_dist[i].s[2] = 99999999.f; // init op een groot getal anders is 0 altijd de min afstand
     }
 
+    // Run until exit
     int is_done = 0;
     while (!is_done)
     {
-        is_done = render_point_cloud(host_pos, length);
+        is_done = render_point_cloud(host_pos, length); // GUI
         time_measure_start("simulation step");
         simulate_gravity(host_pos, host_speed, host_dist, gpu_pos, gpu_speed, gpu_dist, kernel_pos, kernel_speed, length, error_pos, error_speed);
-		for(int k=0; k < length; k++) {
+        // DEBUG
+        for(int k=0; k < length; k++) {
 			printf("gem= %f, max= %f, min= %f\n", host_dist[k].s[0]/length, host_dist[k].s[1], host_dist[k].s[2]);
 		}
         time_measure_stop_and_print("simulation step");
